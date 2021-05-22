c2c2abc292cd419dd2efc3420ebf709e4c638f49
AmandaJisoo <amandapark8988@gmail.com>
2021-05-22 15:38:54 -0700

    now second activity is showing proerly

--- app/src/main/java/com/example/buddybase/SurveyActivity.kt
+++ app/src/main/java/com/example/buddybase/SurveyActivity.kt
@@ -1,6 +1,13 @@
 package com.example.buddybase
 
+import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class SurveyActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_survey);
+        Log.i("SurveyActivity", "hello works")
+    }
 }
