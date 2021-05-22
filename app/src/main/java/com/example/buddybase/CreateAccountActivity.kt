af94d005f70738693e94a80f5b5ed850eb523463
AmandaJisoo <amandapark8988@gmail.com>
2021-05-22 16:49:53 -0700

    can't get username but able to get full name and email

--- app/src/main/java/com/example/buddybase/CreateAccountActivity.kt
+++ app/src/main/java/com/example/buddybase/CreateAccountActivity.kt
@@ -1,17 +1,19 @@
 package com.example.buddybase
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.facebook.*
+import com.facebook.AccessToken
 import com.facebook.appevents.AppEventsLogger
 import com.facebook.login.LoginResult
 import com.facebook.login.widget.LoginButton
 import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
+
 
 class CreateAccountActivity : AppCompatActivity() {
     var callbackManager: CallbackManager? = null
@@ -57,15 +59,76 @@ class CreateAccountActivity : AppCompatActivity() {
     }
 
     private fun handleFacebookAccessToken(token: AccessToken) {
-        Log.i("CreateAccountActivity", "handleFacebookAccessToken:" + token)
 
+        //GRAPH API APPROACH 1
+        //console result:
+        // {"name":"Amanda Jisoo Park","id":"323716089126541"}
+        //{Response:  responseCode: 200, graphObject: {"name":"Amanda Jisoo Park","id":"323716089126541"}, error: null}
+        val batch = GraphRequestBatch(
+            GraphRequest.newMeRequest(
+                token
+            ) { jsonObject, response ->
+                Log.i("here", jsonObject.toString())
+                Log.i("here", response.toString())
+
+            },
+            GraphRequest.newMyFriendsRequest(
+                token
+            ) { jsonArray, response ->
+                // Application code for users friends
+            }
+        )
+        batch.addCallback {
+            // Application code for when the batch finishes
+        }
+
+        batch.executeAsync();
+
+        //APPROCH22 -graph  https://developers.facebook.com/docs/graph-api/reference/user/
+//        GraphRequest(
+//            AccessToken.getCurrentAccessToken(),
+//            "/{person-id}/",
+//            null,
+//            HttpMethod.GET,
+//            GraphRequest.Callback {response -> Log.i("person_id", response.toString())}
+//        ).executeAsync()
+
+
+        //APPROCH3 firebase
+        //console result:
+        //name: Amanda Jisoo Park
+        ///email: apark8988@gmail.com
         val credential = FacebookAuthProvider.getCredential(token.token)
         firebaseAuth!!.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
                     Log.d("CreateAccountActivity", "signInWithCredential:success")
+                    Log.i("token", token.token.toString())
                     val user = firebaseAuth!!.currentUser
+                    user?.let {
+                        for (profile in it.providerData) {
+                            // Id of the provider (ex: google.com)
+                            val providerId = profile.providerId
+
+                            // UID specific to the provider
+                            val uid = profile.uid
+                            val justID = profile.providerId
+                            Log.i("justID", justID)
+
+                            // Name, email address
+                            val name = profile.displayName
+                            val email = profile.email
+                            if (name != null) {
+                                Log.i("name", name)
+                            }
+                            Log.i("uid", uid)
+                            if (email != null) {
+                                Log.i("email", email)
+                            }
+                        }
+                    }
+
                     startActivity(Intent(this@CreateAccountActivity, SurveyActivity::class.java))
                 } else {
                     // If sign in fails, display a message to the user.
@@ -75,4 +138,8 @@ class CreateAccountActivity : AppCompatActivity() {
                 }
             }
     }
+
+
+
+
 }
