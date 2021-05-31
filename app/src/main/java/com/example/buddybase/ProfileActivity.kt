0befb9c1606724b1b0004916d401b0c37a56c1f8
Ryan Lee <ralee478.school@gmail.com>
2021-05-31 14:58:32 -0700

    initial test code complete, android emulator crashing though

--- app/src/main/java/com/example/buddybase/ProfileActivity.kt
+++ app/src/main/java/com/example/buddybase/ProfileActivity.kt
@@ -2,10 +2,48 @@ package com.example.buddybase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.buddybase.databinding.ActivityProfileBinding
+import com.google.firebase.firestore.FirebaseFirestore
+
+// ID for "Bill Gates"
+private const val TEST_DATA = "5uuWVzvVphYMEX8XMy5d"
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+    private lateinit var personDataTemp: Map<String, Any>
+    private lateinit var failMsg: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+
+        val firestore = FirebaseFirestore.getInstance()
+        val docRef = firestore.collection("Users").document(TEST_DATA)
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    personDataTemp = document.data as Map<String, Any>
+                } else {
+                    failMsg = "Doc doesn't exist"
+                }
+            }
+            .addOnFailureListener{ exception ->
+                Log.d("uhOh", "get failed with", exception)
+            }
+
+
+        binding = ActivityProfileBinding.inflate(layoutInflater).apply { setContentView(root) }
+
+        with(binding) {
+            tvName.text = "${personDataTemp["FullName"]}"
+            tvAnimalPreference.text =
+                "Enjoys the company of ${personDataTemp["Q_Pet"]}"
+            tvMusicPreferences.text = "Likes ${personDataTemp["Q_Music"].toString()}"
+            tvShowPreferences.text = "Would watch ${personDataTemp["Q_Show"]}"
+            tvPersonalityType.text = "Acts ${personDataTemp["Q_Personality"].toString()}"
+            tvFoodPreferences.text = "Feels like eating something ${personDataTemp["Q_Taste"]}"
+            tvAttitude.text = "Looking for a friend who is ${personDataTemp["Q_FriendType"].toString()}"
+        }
     }
 }
