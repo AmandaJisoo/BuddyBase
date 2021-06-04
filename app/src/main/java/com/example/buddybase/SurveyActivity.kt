b933cd40d315016d31ca19ff00f50ebdaeae6aa0
eugenelim9 <euglim9@gmail.com>
2021-06-04 01:18:35 -0700

    delete log in surveyactivity

--- app/src/main/java/com/example/buddybase/SurveyActivity.kt
+++ app/src/main/java/com/example/buddybase/SurveyActivity.kt
@@ -14,7 +14,6 @@ class SurveyActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.i("SurveyActivity", "hello works")
 
         binding = ActivitySurveyBinding.inflate(layoutInflater).apply { setContentView(root) }
         this.manager = userApp.userManager
