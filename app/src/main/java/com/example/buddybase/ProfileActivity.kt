9b3f8f3e6a91b7944656be0d7124ef0d5399ca20
Ryan Lee <ralee478.school@gmail.com>
2021-06-03 17:44:14 -0700

    added image loading, waiting on images to be uploaded

--- app/src/main/java/com/example/buddybase/ProfileActivity.kt
+++ app/src/main/java/com/example/buddybase/ProfileActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.util.Log
 
 import androidx.lifecycle.lifecycleScope
+import coil.load
 import com.example.buddybase.databinding.ActivityProfileBinding
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.launch
 
@@ -21,39 +23,9 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        lifecycleScope.launch {
-//            runCatching {
-//                val firestore = FirebaseFirestore.getInstance()
-//                val docRef = firestore.collection("Users").document(TEST_DATA)
-//                docRef.get()
-//                    .addOnSuccessListener { document ->
-//                        if (document != null) {
-//                            personDataTemp = document.data as Map<String, Any>
-//                        } else {
-//                            failMsg = "Doc doesn't exist"
-//                        }
-//                    }
-//                    .addOnFailureListener{ exception ->
-//                        Log.d("uhOh", "get failed with", exception)
-//                    }
-//            }.onFailure {
-//                failMsg = "Failed"
-//            }
-//        }
-
         binding = ActivityProfileBinding.inflate(layoutInflater).apply { setContentView(root) }
 
         with(binding) {
-//            if (this@ProfileActivity::personDataTemp.isInitialized) {
-//                tvName.text = "${personDataTemp["FullName"]}"
-//                tvAnimalPreference.text =
-//                    "Enjoys the company of ${personDataTemp["Q_Pet"]}"
-//                tvMusicPreferences.text = "Likes ${personDataTemp["Q_Music"].toString()}"
-//                tvShowPreferences.text = "Would watch ${personDataTemp["Q_Show"]}"
-//                tvPersonalityType.text = "Acts ${personDataTemp["Q_Personality"].toString()}"
-//                tvFoodPreferences.text = "Feels like eating something ${personDataTemp["Q_Taste"]}"
-//                tvAttitude.text = "Looking for a friend who is ${personDataTemp["Q_FriendType"].toString()}"
-//            }
 
             btnTempPull.setOnClickListener {
                 loadData(binding)
@@ -61,6 +33,7 @@ class ProfileActivity : AppCompatActivity() {
         }
     }
 
+    // TODO: Copy this function so that it gets called when profile is tapped from the tab bar
     private fun loadData(binding: ActivityProfileBinding) {
         lifecycleScope.launch {
             runCatching {
@@ -70,19 +43,27 @@ class ProfileActivity : AppCompatActivity() {
                     .addOnSuccessListener { document ->
                         if (document != null) {
                             personDataTemp = document.data as Map<String, Any>
+
+                            // Debug code to look at where prof pic image is supposed to be
+                            val picReference: DocumentReference = personDataTemp["ImageProfilePic"] as DocumentReference
+                            picReference.get()
+                                .addOnSuccessListener { document ->
+                                    if (document != null) {
+                                        Log.d("loadDataMsg", document.getData().toString())
+                                    } else {
+                                        Log.d("loadDataMsg", "pic didn't retrieve")
+                                    }
+                                }
+
                             with(binding) {
+                                //ivVariableProfPic.load(personDataTemp["ImageProfilePic"])
                                 tvName.text = "${personDataTemp["FullName"]}"
-                                tvAnimalPreference.text =
-                                    "Enjoys the company of ${personDataTemp["Q_Pet"]}"
-                                tvMusicPreferences.text =
-                                    "Likes ${personDataTemp["Q_Music"].toString()}"
-                                tvShowPreferences.text = "Would watch ${personDataTemp["Q_Show"]}"
-                                tvPersonalityType.text =
-                                    "Acts ${personDataTemp["Q_Personality"].toString()}"
-                                tvFoodPreferences.text =
-                                    "Feels like eating something ${personDataTemp["Q_Taste"]}"
-                                tvAttitude.text =
-                                    "Looking for a friend who is ${personDataTemp["Q_FriendType"].toString().substring(1, personDataTemp["Q_FriendType"].toString().length - 1)}"
+                                tvVariableAnimal.text = "${personDataTemp["Q_Pet"]}"
+                                tvVariableMusic.text = "${personDataTemp["Q_Music"].toString().drop(1).dropLast(1)}"
+                                tvVariableShow.text = "Would watch ${personDataTemp["Q_Show"]}"
+                                tvPersonalityType.text = "${personDataTemp["Q_Personality"].toString().drop(1).dropLast(1)}"
+                                tvVariableFood.text = "${personDataTemp["Q_Taste"]}"
+                                tvVariableAttitude.text = "${personDataTemp["Q_FriendType"].toString().drop(1).dropLast(1)}"
                             }
                         } else {
                             failMsg = "Doc doesn't exist"
